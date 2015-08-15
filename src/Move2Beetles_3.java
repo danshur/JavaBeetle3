baabc58845043be5a35cb8af367b97fcfc99865d
Dan Shur <danshur@gmail.com>
2015-08-15 15:18:41 -0700

    made some comments

--- src/Move2Beetles_3.java
+++ src/Move2Beetles_3.java
@@ -1,12 +1,13 @@
 
 public class Move2Beetles_3 {
 	public static void main(String[] args) {
-
+		//create beetles and set position if needed
 		Beetle bailey = new Beetle("green");
 		Beetle ed = new Beetle("red");
 		ed.getPosition();
 		ed.turn(180);
 		
+		//draw beetles
 		drawOctagon(bailey);
 		drawOctagon(ed);
 	}
